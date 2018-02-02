16bbc5f5bb4fe9e4c750c2df9512a8c0ef5054dc
topolewskidev <marcin.topolewski.dev@gmail.com>
2018-02-02 21:51:21 +0100

    Delete useless node modules and angular compnents

--- TrackYourBudget/ClientApp/app/app.shared.module.ts
+++ TrackYourBudget/ClientApp/app/app.shared.module.ts
@@ -7,16 +7,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { BudgetComponent } from './components/budget/budget.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
 import { AddExpenseComponent } from "./components/addexpense/addexpense.component";
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
         AddExpenseComponent,
         BudgetComponent
     ],
@@ -28,8 +24,6 @@ import { AddExpenseComponent } from "./components/addexpense/addexpense.componen
         RouterModule.forRoot([
             { path: '', redirectTo: 'budget', pathMatch: 'full' },
             { path: 'budget', component: BudgetComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
             { path: 'expenses/add', component: AddExpenseComponent },
             { path: '**', redirectTo: 'budget' }
         ])
